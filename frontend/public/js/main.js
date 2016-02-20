1a234ce7679bd2abeb9ead3f4a3a3d4d569d98bc
Angus Innes <Angus@Angus-Inness-MacBook-Pro-4.local>
2016-02-20 16:45:45 +0000

    loop to populate hospital locations working

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
 	console.log("hello")
 
-	//initialise map
+	//make map appear on page
+	// target dom element and populate 
+	// specify what segment of map and its style
 	var googleMapTest = function() {
 
 		// instantiate map object
@@ -20,53 +22,139 @@ $(document).ready(function(){
 		});
 	}
 
-// add a second mar
 	googleMapTest();
 	// array has to be defined before it can be pushed into
 	var markersArray = [];
 
-	// function to add marker to map
-	function addPlaceMarker(name,lat,lng) {
-		// making new point demarked by latitude and longitude
-	  var latlng = new google.maps.LatLng(lat,lng);
+	// // function to add marker to map
+	// function addPlaceMarker(name,lat,lng) {
+	// 	// making new point demarked by latitude and longitude
+	//   var latlng = new google.maps.LatLng(lat,lng);
 
-	  // var contentString = "<p>" + name + "</p>";
-	  // 	// make marker appear above place
-	  // var infowindow = new google.maps.InfoWindow({
-	  //     content: contentString
-	  // });
-		//console.log(latlng)
-	  	// adding marker
-	  	console.log(marker)
-	  var marker = new google.maps.Marker(
-	  {
-	  	//position on map
-	    position: latlng,
-	    //
-	    map: map,
-	    title: name
-	  });
-	  console.log(marker)
-	  // push to array
-	  markersArray.push(marker);
-	  console.log(markersArray)
-	  // is coming out
+	//   // var contentString = "<p>" + name + "</p>";
+	//   // 	// make marker appear above place
+	//   // var infowindow = new google.maps.InfoWindow({
+	//   //     content: contentString
+	//   // });
+	// 	//console.log(latlng)
+	//   	// adding marker
+	//   	console.log(marker)
+	//   var marker = new google.maps.Marker(
+	//   {
+	//   	//position on map
+	//     position: latlng,
+	//     //
+	//     map: map,
+	//     title: name
+	//   });
+	//   console.log(marker)
+	//   // push to array
+	//   markersArray.push(marker);
+	//   console.log(markersArray)
+	//   // is coming out
 
-	  // // infowindow displays on mouseover
-	  // marker.addListener('mouseover', function() {
-	  //   infowindow.open(map, marker);
-	  // });
+	//   // // infowindow displays on mouseover
+	//   // marker.addListener('mouseover', function() {
+	//   //   infowindow.open(map, marker);
+	//   // });
 
-	  // marker.addListener('mouseout', function() {
-	  //   infowindow.close();
-	  // });
+	//   // marker.addListener('mouseout', function() {
+	//   //   infowindow.close();
+	//   // });
 
-	  }
-	  // has added london to page!
-	  addPlaceMarker("bob",51.52864165, -0.10179430)
-	  // add a second marker
-	  addPlaceMarker("testmarker2",41.52864165, -0.10179430)
+	//   }
+	//   // has added london to page!
+	//   addPlaceMarker("bob",51.52864165, -0.10179430)
+	//   // add a second marker
+	//   addPlaceMarker("testmarker2",41.52864165, -0.10179430)
 
-	  // make a loop to add soft 
+	//   // make a loop to add multiple places at same time
+	//   addPlaceMarker("bob",51.52864165, -0.10179430)
+
+	 // var names= ["bob", "susy", "john"]
+	 // var latitude= [51.52864165, 41.52864165, 31.52864165]
+	 // var longitude = [-0.10179430]
+
+	 // what are you trying to do?
+	 // loop over array of data and pass as argument to function addPlaceMarker
+	 // not necessary right now
+	 addPlaceMarkersArray = [{name: "bob",
+	 						  lat: 51.52864165,
+	 						  lng: -0.10179430}
+	 						  ,
+	 						  {name: "testmarker2",
+	 						  lat: 41.52864165,
+	 						  lng: -0.10179430}
+	 						  ,
+	 						  {name: "testmarker3",
+	 						   lat: 31.52864165,
+	 						   lng: -0.10179430}
+	 						   ]
+	 //console.log(addPlaceMarkersArray[0])
+
+	var testmarkeradder = function(){
+
+
+
+		function addPlaceMarker(name,lat,lng) {
+			// making new point demarked by latitude and longitude
+		  var latlng = new google.maps.LatLng(lat,lng);
+
+		  // var contentString = "<p>" + name + "</p>";
+		  // 	// make marker appear above place
+		  // var infowindow = new google.maps.InfoWindow({
+		  //     content: contentString
+		  // });
+			//console.log(latlng)
+		  	// adding marker
+		  	//console.log(marker)
+		  var marker = new google.maps.Marker(
+		  {
+		  	//position on map
+		    position: latlng,
+		    //
+		    map: map,
+		    title: name
+		  });
+		  //console.log(marker)
+		  // push to array
+		  markersArray.push(marker);
+		  console.log(markersArray)
+		  // is coming out
+
+		  // // infowindow displays on mouseover
+		  // marker.addListener('mouseover', function() {
+		  //   infowindow.open(map, marker);
+		  // });
+
+		  // marker.addListener('mouseout', function() {
+		  //   infowindow.close();
+		  // });
+
+		  }
+
+		var markerloop = function(){ for(i=0; i<addPlaceMarkersArray.length; i++){
+		 	//console.log(addPlaceMarkersArray[i])
+		 	name = (addPlaceMarkersArray[i].name)
+
+		 	lat = (addPlaceMarkersArray[i].lat)
+		 	lng = (addPlaceMarkersArray[i].lng)
+		 	//console.log(name, lat, lng)
+		 	addPlaceMarker(name, lat, lng)
+		 }
+		}
+
+		markerloop()
+		//console.log(name) // returns the last name made by loop
+		//console.log(markerloop) // prints the function you want what it makes
+
+		// gives object with name, lat, lng properties
+
+		// need to pass these into addPlaceMarker function
+
+	}
+
+	testmarkeradder()
+
 
 });
